f4e822312de9e58fd78e3928a0116c6c0aeabb87
DmytroSoldatov <cyberdexx@gmail.com>
2023-03-15 09:57:58 +0200

    remove text-html

--- src/utils/helpers/text.ts
+++ src/utils/helpers/text.ts
@@ -1,11 +1,10 @@
 import { Text } from '@pixi/text';
 import { BitmapText } from '@pixi/text-bitmap';
-import { HTMLText } from '@pixi/text-html';
 
-export type PixiText = Text | BitmapText | HTMLText;
+export type PixiText = Text | BitmapText;
 export type AnyText = string | number | PixiText;
 
-export function getTextView(text: AnyText): Text | BitmapText | HTMLText
+export function getTextView(text: AnyText): Text | BitmapText
 {
     if (typeof text === 'string' || typeof text === 'number')
     {
