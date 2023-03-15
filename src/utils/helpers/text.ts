166feab2f62cff91b8bdb1a1f570d6273a24f401
DmytroSoldatov <cyberdexx@gmail.com>
2023-03-15 10:09:08 +0200

    add html text

--- src/utils/helpers/text.ts
+++ src/utils/helpers/text.ts
@@ -1,10 +1,11 @@
 import { Text } from '@pixi/text';
 import { BitmapText } from '@pixi/text-bitmap';
+import { HTMLText } from '@pixi/text-html';
 
-export type PixiText = Text | BitmapText;
+export type PixiText = Text | BitmapText | HTMLText;
 export type AnyText = string | number | PixiText;
 
-export function getTextView(text: AnyText): Text | BitmapText
+export function getTextView(text: AnyText): Text | BitmapText | HTMLText
 {
     if (typeof text === 'string' || typeof text === 'number')
     {
