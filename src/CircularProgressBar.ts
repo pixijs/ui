f08ce096ea839819992086710ba6f3b3143b2489
Dmytro Soldatov <cyberdexx@gmail.com>
2023-07-15 18:37:40 +0300

    fix docs

--- src/CircularProgressBar.ts
+++ src/CircularProgressBar.ts
@@ -16,13 +16,14 @@ export type MaskedProgressBarOptions = {
 /**
  * Creates a Circular ProgressBar.
  * @example
- * new ProgressBar({
- *     fillColor: '#00b1dd',
- *     borderColor: '#FFFFFF',
- *     backgroundColor: '#fe6048',
- *     value: 50,
- *     radius: 25,
- *     border: 3,
+ * new CircularProgressBar({
+ *    backgroundColor: 0x000000,
+ *    backgroundAlpha: 0.5,
+ *    lineWidth: 10,
+ *    fillColor: 0xFFFFFF,
+ *    radius: 50,
+ *    value: 50,
+ *    cap: 'round'
  * });
  */
 export class CircularProgressBar extends Container
@@ -36,6 +37,18 @@ export class CircularProgressBar extends Container
     /** Container, that holds all inner views. */
     innerView = new Container();
 
+    /**
+     * Creates a Circular ProgressBar.
+     * @param { number } options - Options object to use.
+     * @param { number } options.backgroundColor - Background color.
+     * @param { number } options.fillColor - Fill color.
+     * @param { number } options.lineWidth - Line width.
+     * @param { number } options.radius - Radius.
+     * @param { number } options.value - Progress value.
+     * @param { number } options.backgroundAlpha - Background alpha.
+     * @param { number } options.fillAlpha - Fill alpha.
+     * @param { 'butt' | 'round' | 'square' } options.cap - Line cap.
+     */
     constructor(options?: MaskedProgressBarOptions)
     {
         super();
@@ -82,7 +95,10 @@ export class CircularProgressBar extends Container
         }).drawCircle(0, 0, radius);
     }
 
-    /** Set progress value. */
+    /**
+     * Set progress value.
+     * @param { number } value - Progress value.
+     */
     set progress(value: number)
     {
         if (value > 100)
@@ -126,7 +142,10 @@ export class CircularProgressBar extends Container
             .arc(0, 0, radius, (0 - 90 + startAngle) * DEG_TO_RAD, (0 - 90 + startAngle + endAngle) * DEG_TO_RAD);
     }
 
-    /** Current progress value. */
+    /**
+     * Current progress value.
+     * @returns { number } - Progress value.
+     */
     get progress(): number
     {
         return this._progress;
