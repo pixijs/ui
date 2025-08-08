fcbf1391fc4bfbc490dad3f71da0fd463b14adc1
Alexander Farber <farber72@outlook.de>
2025-08-08 15:53:57 +0100

    Add Jest testing setup with latest dependencies

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/** @type {import('jest').Config} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/', '/docs/'],
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', {
+            tsconfig: {
+                module: 'ESNext',
+                esModuleInterop: true,
+            },
+            diagnostics: false,
+        }],
+        '\\.(png|jpg|gif|svg)$': '<rootDir>/tests/file-mock.js'
+    },
+    moduleNameMapper: {
+        '^~/(.*)$': '<rootDir>/src/$1'
+    },
+    testMatch: ['**/?(*.)+(test|spec)\\.ts'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!<rootDir>/src/**/*.stories.ts'
+    ],
+    coverageDirectory: '<rootDir>/coverage',
+    testTimeout: 10000,
+};
---
+++ tests/file-mock.js
@@ -0,0 +1,2 @@
+// Mock file imports for Jest (images, assets, etc.)
+module.exports = 'test-file-stub';
