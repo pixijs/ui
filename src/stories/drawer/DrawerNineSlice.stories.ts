555ecc7000a3a9dfcebb8f2fa650b6fd6b4da3b5
Alexander Farber <farber72@outlook.de>
2025-11-20 22:00:43 +0100

    Reorder stories

--- src/stories/drawer/DrawerNineSlice.stories.ts
+++ src/stories/drawer/DrawerNineSlice.stories.ts
@@ -90,6 +90,75 @@ export const NineSliceBottom = {
     args: getDefaultArgs(args),
 };
 
+export const NineSliceTop = {
+    render: (args: Args, ctx: StoryContext) =>
+        new PixiStory({
+            context: ctx,
+            init: async (view) =>
+            {
+                const {
+                    width,
+                    height,
+                    padding,
+                    backdropColor,
+                    backdropAlpha,
+                    contentColor,
+                    closeOnBackdropClick,
+                    swipeToClose,
+                } = args;
+
+                await preload(['button_gray.png']);
+
+                const drawer = new Drawer({
+                    background: Texture.from('button_gray.png'),
+                    nineSliceSprite: [25, 20, 25, 20],
+                    backdropColor: getColor(backdropColor),
+                    backdropAlpha,
+                    content: new Text({
+                        text: 'Top drawer with NineSliceSprite background.\n\nSwipe up to close.',
+                        style: {
+                            ...defaultTextStyle,
+                            fontSize: 16,
+                            align: 'center',
+                            wordWrapWidth: width - (2 * padding),
+                            wordWrap: true,
+                            fill: getColor(contentColor),
+                        },
+                    }),
+                    width,
+                    height,
+                    padding,
+                    position: 'top',
+                    closeOnBackdropClick,
+                    swipeToClose,
+                    animations: {
+                        open: { props: {}, duration: 300 },
+                        close: { props: {}, duration: 300 },
+                    },
+                });
+
+                drawer.onClose.connect(() =>
+                {
+                    action('onClose')('Drawer closed');
+                    setTimeout(() => drawer.open(), 1000);
+                });
+
+                // Set screen size for proper positioning
+                const canvas = document.getElementById('storybook-root');
+
+                if (canvas)
+                {
+                    drawer.setScreenSize(canvas.offsetWidth, canvas.offsetHeight);
+                }
+
+                view.addChild(drawer);
+                drawer.open();
+            },
+            resize: centerView,
+        }),
+    args: getDefaultArgs(args),
+};
+
 export const NineSliceLeft = {
     render: (args: Args, ctx: StoryContext) =>
         new PixiStory({
@@ -160,75 +229,6 @@ export const NineSliceLeft = {
 };
 
 export const NineSliceRight = {
-    render: (args: Args, ctx: StoryContext) =>
-        new PixiStory({
-            context: ctx,
-            init: async (view) =>
-            {
-                const {
-                    width,
-                    height,
-                    padding,
-                    backdropColor,
-                    backdropAlpha,
-                    contentColor,
-                    closeOnBackdropClick,
-                    swipeToClose,
-                } = args;
-
-                await preload(['button_gray.png']);
-
-                const drawer = new Drawer({
-                    background: Texture.from('button_gray.png'),
-                    nineSliceSprite: [25, 20, 25, 20],
-                    backdropColor: getColor(backdropColor),
-                    backdropAlpha,
-                    content: new Text({
-                        text: 'Right drawer with NineSliceSprite background.\n\nSwipe right to close.',
-                        style: {
-                            ...defaultTextStyle,
-                            fontSize: 16,
-                            align: 'center',
-                            wordWrapWidth: width - (2 * padding),
-                            wordWrap: true,
-                            fill: getColor(contentColor),
-                        },
-                    }),
-                    width,
-                    height,
-                    padding,
-                    position: 'right',
-                    closeOnBackdropClick,
-                    swipeToClose,
-                    animations: {
-                        open: { props: {}, duration: 300 },
-                        close: { props: {}, duration: 300 },
-                    },
-                });
-
-                drawer.onClose.connect(() =>
-                {
-                    action('onClose')('Drawer closed');
-                    setTimeout(() => drawer.open(), 1000);
-                });
-
-                // Set screen size for proper positioning
-                const canvas = document.getElementById('storybook-root');
-
-                if (canvas)
-                {
-                    drawer.setScreenSize(canvas.offsetWidth, canvas.offsetHeight);
-                }
-
-                view.addChild(drawer);
-                drawer.open();
-            },
-            resize: centerView,
-        }),
-    args: getDefaultArgs(args),
-};
-
-export const NineSliceTop = {
     render: (args: Args, ctx: StoryContext) =>
         new PixiStory({
             context: ctx,
